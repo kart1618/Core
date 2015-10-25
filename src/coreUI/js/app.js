c5ad11654b828123fdafedcafea0ebf68050f77e
kart1618 <kart1618@gmail.com>
2015-10-24 19:11:45 -0500

    First Commit

---
+++ src/coreUI/js/app.js
@@ -0,0 +1,35 @@
+(function(){
+    'use strict';
+
+    angular.module('newspaper', ['ngMaterial'])
+     .controller("newspaperController", ['$scope', '$http', '$mdSidenav', function ($scope, $http, $mdSidenav) {
+           $scope.documents = [];
+           $scope.isSearching = false;
+           $scope.search = function() {
+            $scope.isSearching = true;
+            $http({
+                method: "GET",
+                url: "http://api.nytimes.com/svc/search/v2/articlesearch.json",
+                params: {
+                    "api-key": "b9cea2a4b8aecad8b374d88b275cc59a:10:70162025",
+                    "q": $scope.searchTerm
+                }
+            }).success (function(data) {
+                $scope.documents = data.response.docs;
+                console.log($scope.documents[0].web_url);
+                $scope.isSearching = false;
+
+            }).error (function(error){
+                console.log(error);
+                $scope.isSearching = false;
+            });
+            console.log($scope.searchTerm);
+           };
+          $mdSidenav('left').open();
+          $scope.openLeftMenu = function() {
+            $mdSidenav('left').toggle();
+            console.log("HELLO");
+          };
+          $scope.openLeftMenu();
+     }]);
+})();
