2c127cfd24bc70838017f9437204232dd2a594c7
kart1618 <kart1618@gmail.com>
2015-11-01 00:00:06 -0500

    add more functionality to the addTab directive (formarly tabContent directive). Also refactored code to better conform to angular best practices (more changes with regards to this in future).

--- src/coreUI/directives/common/addTab/addTab.js
+++ src/coreUI/directives/common/addTab/addTab.js
@@ -1,10 +1,48 @@
 "use strict";
 
 angular.module("core")
-    .directive("tabContent", function () {
+    .directive("addTab", function () {
         return {
             restrict: "E",
-            templateUrl: "directives/common/tabContent/addTab.html",
-            scope: true
-        }
-    });
+            templateUrl: "directives/common/addTab/addTab.html",
+            scope: {
+                tabName: "@"
+            },
+            controller: ['$scope', '$http', '$mdSidenav', function ($scope, $http, $mdSidenav) {
+                $scope.documents = [];
+                $scope.isSearching = false;
+//                function init() {
+//                    $scope.tabHeading = angular.copy($scope.tabName);
+//                }
+//                init();
+                $scope.search = function () {
+                    $scope.isSearching = true;
+                    $http({
+                        method: "GET",
+                        url: "http://api.nytimes.com/svc/search/v2/articlesearch.json",
+                        params: {
+                            "api-key": "b9cea2a4b8aecad8b374d88b275cc59a:10:70162025",
+                            "q": $scope.searchTerm,
+                            "page": 1
+                        }
+                    }).success(function (data) {
+                        $scope.documents = data.response.docs;
+                        console.log($scope.documents[0].web_url);
+                        console.log($scope.documents[0].snippet);
+                        $scope.isSearching = false;
+
+                    }).error(function (error) {
+                        console.log(error);
+                        $scope.isSearching = false;
+                    });
+                    console.log($scope.searchTerm + $scope.tabName);
+                };
+                $mdSidenav('right').open();
+                $scope.openRightMenu = function () {
+                    $mdSidenav('right').toggle();
+                    console.log("HELLO");
+                };
+                $scope.openRightMenu();
+            }]
+    }
+});
