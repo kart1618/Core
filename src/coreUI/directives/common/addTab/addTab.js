454be007626599c6c565e02132b58ec7c2a693c4
karthik-vasishta.ramesh <karthik-vasishta.ramesh@hp.com>
2015-11-05 20:44:26 -0600

    Added support for the guardian API. This is still a work in progress. A directive to display results of a specific api has to be built. This directive will be used in addTab.html (for now).

--- src/coreUI/directives/common/addTab/addTab.js
+++ src/coreUI/directives/common/addTab/addTab.js
@@ -11,7 +11,7 @@ angular.module("core")
             controller: ['$scope', '$filter', '$http', '$mdSidenav', function ($scope, $filter, $http, $mdSidenav) {
                 $scope.documents = [];
                 $scope.isSearching = false;
-                $scope.nytimessearchOptions = {
+                $scope.searchOptions = {
                     show: false,
                     page: 1
                 };
@@ -19,35 +19,64 @@ angular.module("core")
                 $scope.search = function () {
 
                     if ($scope.tabName === "The New York Times") {
-                        console.log("HERe");
+                        console.log("Here");
+                        $scope.isSearching = true;
+
+                        $http({
+                            method: "GET",
+                            url: "http://api.nytimes.com/svc/search/v2/articlesearch.json",
+                            params: {
+                                "api-key": "b9cea2a4b8aecad8b374d88b275cc59a:10:70162025",
+                                "q": $scope.searchTerm,
+                                "page": $scope.searchOptions.page,
+                                "hl": true
+                            }
+                        }).success(function (data) {
+                            $scope.documents = data.response.docs;
+                            $scope.searchOptions.show = true;
+                            $scope.isSearching = false;
+                            //                        console.log($scope.documents[0].web_url);
+                            //                        console.log($scope.documents[0].snippet);
+                            console.log($scope.documents);
+
+                        }).error(function (error) {
+                            console.log(error);
+                            $scope.isSearching = false;
+                        });
+                        console.log($scope.searchTerm + $scope.tabName);
+                    } else if ($scope.tabName === "The Guardian") {
+                        //////
+                        $scope.isSearching = true;
+                        $http({
+                            method: "GET",
+                            url: "http://content.guardianapis.com/search",
+                            params: {
+                                "api-key": "5qxrs3tb5vsaa5yujzuxbdyv",
+                                "q": $scope.searchTerm,
+                                "page": $scope.searchOptions.page,
+                                "show-fields": "trailText"
+                            }
+                        }).success(function (data) {
+                            $scope.documents = data.response.results;
+                            console.log($scope.documents);
+                            $scope.searchOptions.show = true;
+                            $scope.isSearching = false;
+                            //                        console.log($scope.documents[0].web_url);
+                            //                        console.log($scope.documents[0].snippet);
+                            console.log(data.response.pageSize);
+
+                        }).error(function (error) {
+                            console.log(error);
+                            $scope.isSearching = false;
+                        });
+                        console.log($scope.searchTerm + " " + $scope.tabName);
+                        //////
                     }
-                    $scope.isSearching = true;
-
-                    $http({
-                        method: "GET",
-                        url: "http://api.nytimes.com/svc/search/v2/articlesearch.json",
-                        params: {
-                            "api-key": "b9cea2a4b8aecad8b374d88b275cc59a:10:70162025",
-                            "q": $scope.searchTerm,
-                            "page": $scope.nytimessearchOptions.page,
-                            "hl": true
-                        }
-                    }).success(function (data) {
-                        $scope.documents = data.response.docs;
-                        $scope.nytimessearchOptions.show = true;
-                        $scope.isSearching = false;
-//                        console.log($scope.documents[0].web_url);
-//                        console.log($scope.documents[0].snippet);
-                        console.log(data.response.meta.hits);
-
-                    }).error(function (error) {
-                        console.log(error);
-                        $scope.isSearching = false;
-                    });
-                    console.log($scope.searchTerm + $scope.tabName);
                 };
                 $mdSidenav('right').open();
                 $scope.openRightMenu = function () {
+                    $scope.documents = "";
+                    console.log("Documents cleared!");
                     $mdSidenav('right').toggle();
                     console.log("HELLO");
                 };
