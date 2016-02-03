3aa3d7af243024ecc54004ce7b98ce97ce7e1b44
kart1618 <kart1618@gmail.com>
2016-02-03 00:57:38 -0600

    Added dependency on spring-boot in pom.xml. Also added sample code to get started with Spring Boot.

--- coreservices/src/main/java/coreservices/App.java
+++ coreservices/src/main/java/coreservices/App.java
@@ -1,13 +1,25 @@
 package coreservices;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 /**
  * Hello world!
  *
  */
+
+@RestController
+@EnableAutoConfiguration
 public class App 
 {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
