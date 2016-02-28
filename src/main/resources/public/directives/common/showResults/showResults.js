d6756cfc807eff22d5f704dda03e626978dbebd4
karthik-vasishta.ramesh <karthik-vasishta.ramesh@hp.com>
2016-02-27 20:12:51 -0600

    Changes to show a specific icon, based on sentiment verdict [WIP]

--- src/main/resources/public/directives/common/showResults/showResults.js
+++ src/main/resources/public/directives/common/showResults/showResults.js
@@ -12,38 +12,40 @@ angular.module("core")
 
                 $scope.displayResults = [false, false, false];
                 $scope.url = "";
-                var promise, success, error;
+                $scope.sentimentVerdict;
+                angular.forEach($scope.resultArr, function(value, key) {
 
-                var initialise = function () {
-                     if ($scope.tabName === "The New York Times") {
-                        $scope.displayResults[0] = true;
-                     }
-                     else if ($scope.tabName === "The Guardian") {
-                        $scope.displayResults[1] = true;
-                     }
-                     else if ($scope.tabName === "You Tube") {
-                        $scope.url = "https://www.youtube.com/watch?v=";
-                        $scope.displayResults[2] = true;
-                     }
-                };
-                initialise();
+                });
+                var promise, success, error;
+                if ($scope.tabName === "The New York Times") {
+                    $scope.displayResults[0] = true;
+                }
+                else if ($scope.tabName === "The Guardian") {
+                    $scope.displayResults[1] = true;
+                }
+                else if ($scope.tabName === "You Tube") {
+                    $scope.url = "https://www.youtube.com/watch?v=";
+                    $scope.displayResults[2] = true;
+                }
 
                 $scope.getYouTubeUrl = function (id) {
                     $scope.url = "https://www.youtube.com/watch?v=" + id;
                     return $scope.url;
                 };
 
-                $scope.analyzeSentiment = function (url) {
+                $scope.analyzeSentiment = function (url, document) {
                     $scope.url = url;
                     promise = tabService.hodSentimentAnalysis($scope.url);
-                    success = function(response) {
+                    success = function (response) {
                         console.log(response.data.aggregate);
+                        document.sentimentVerdict = response.data.aggregate.sentiment;
                     };
-                    error = function(error) {
+                    error = function (error) {
                         console.log(error);
                     };
                     promise.then(success, error);
                 };
             }]
         }
-    }]);
+    }])
+;
