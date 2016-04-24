0fdd662d93f94a211253adb668ba506545054aeb
kart1618 <kart1618@gmail.com>
2016-04-24 18:34:26 -0500

    - Added capabilitty to sort search results based several criteria. - Added a search button.

--- src/main/resources/public/controllers/tabController.js
+++ src/main/resources/public/controllers/tabController.js
@@ -5,20 +5,38 @@ angular.module('core')
             $scope.documents = [];
             $scope.isSearching = false;
             $scope.showSearchOptions = false;
-            var nytPageResetCounter = 0;
-            var guardianPageResetCounter = 0;
-            var promise, success, error;
             $scope.tabStringLiterals = $rootScope.literals.tabs;
+            $scope.defaults = {
+                "sortOrder": "newest"
+            };
             $scope.searchOptions = {
                 page: 1
             };
+
+            if ($scope.tabName === "The New York Times") {
+                $scope.sortOrder = ["newest", "oldest"];
+            }
+            else if ($scope.tabName === "The Guardian") {
+                $scope.sortOrder = ["None", "newest", "oldest", "relevance"];
+            }
+            if ($scope.tabName === "You Tube") {
+                $scope.sortOrder = ["Date", "rating", "title", "relevance", "videoCount", "viewCount"];
+            }
+            $scope.selectedSortOrder = $scope.defaults.sortOrder;
             $scope.searchText = "";
+
+            var nytPageResetCounter = 0;
+            var guardianPageResetCounter = 0;
+            var promise, success, error;
+
             var searchNewYorkTimes = function() {
+
                 if (nytPageResetCounter > 0) {
                     nytPageResetCounter = 0;
                     $scope.searchOptions.page = 1;
                 }
-                promise = tabService.nytArticleSearch($scope.searchText, $scope.searchOptions.page);
+
+                promise = tabService.nytArticleSearch($scope.searchText, $scope.searchOptions.page, $scope.selectedSortOrder);
                 success = function(response) {
                     $scope.documents = response.data.response.docs;
                     $scope.showSearchOptions = true;
@@ -26,69 +44,72 @@ angular.module('core')
                 };
                 error = function(error) {
                     $scope.isSearching = false;
-                    };
+                };
                     promise.then(success, error);
-                };
+            };
 
-                var searchGaurdian = function () {
-                    if (guardianPageResetCounter > 0) {
-                        guardianPageResetCounter = 0;
-                        $scope.searchOptions.page = 1;
-                    }
-                    promise = tabService.guardianArticleSearch($scope.searchText, $scope.searchOptions.page);
-                    success = function(response) {
-                        $scope.documents = response.data.response.results;
-                        $scope.showSearchOptions = true;
-                        $scope.isSearching = false;
-                    };
-                    error = function(error) {
-                        $scope.isSearching = false;
-                    };
-                    promise.then(success, error);
-                };
+            var searchGaurdian = function () {
 
-                var searchYoutube = function () {
-                    promise = tabService.youtubeSearch($scope.searchText, "video");
-                    success = function(response) {
-                        $scope.documents = response.data.items;
-                        $scope.isSearching = false;
-                    };
-                    error = function(error) {
-                        $scope.isSearching = false;
-                    };
-                    promise.then(success, error);
-                };
-
-                $scope.search = function (userSearchText) {
-                    $scope.searchText = userSearchText;
-                    $scope.isSearching = true;
-                    $scope.showSearchOptions = false;
-                    if ($scope.tabName === "The New York Times") {
-                        searchNewYorkTimes();
-                    }
-                    else if ($scope.tabName === "The Guardian") {
-                        searchGaurdian();
-                    }
-                    else if ($scope.tabName === "You Tube") {
-                        searchYoutube();
-                    }
-                };
-
-                $mdSidenav('right').open();
-                $scope.openRightMenu = function () {
-                    $scope.documents = "";
-                    $mdSidenav('right').toggle();
-                };
-                $scope.openRightMenu();
-
-                $scope.clearResults = function () {
-                    $scope.documents = "";
-                    $scope.showSearchOptions = false;
-                    if ($scope.tabName === "The New York Times") {
-                        nytPageResetCounter += 1;
-                    }
-                    else if ($scope.tabName === "The Guardian") {
-                        guardianPageResetCounter += 1;
-                    }
+                if (guardianPageResetCounter > 0) {
+                    guardianPageResetCounter = 0;
+                    $scope.searchOptions.page = 1;
                 }
-            }]);
+                promise = tabService.guardianArticleSearch($scope.searchText, $scope.searchOptions.page, $scope.selectedSortOrder);
+                success = function(response) {
+                    $scope.documents = response.data.response.results;
+                    $scope.showSearchOptions = true;
+                    $scope.isSearching = false;
+                };
+                error = function(error) {
+                    $scope.isSearching = false;
+                };
+                promise.then(success, error);
+            };
+
+            var searchYoutube = function () {
+
+                promise = tabService.youtubeSearch($scope.searchText, "video", $scope.selectedSortOrder);
+                success = function(response) {
+                    $scope.documents = response.data.items;
+                    $scope.isSearching = false;
+                };
+                error = function(error) {
+                    $scope.isSearching = false;
+                };
+                promise.then(success, error);
+            };
+
+            $scope.search = function (userSearchText, selectedSortOrder) {
+                $scope.searchText = userSearchText;
+                $scope.selectedSortOrder = selectedSortOrder;
+                $scope.isSearching = true;
+                $scope.showSearchOptions = false;
+                if ($scope.tabName === "The New York Times") {
+                    searchNewYorkTimes();
+                }
+                else if ($scope.tabName === "The Guardian") {
+                    searchGaurdian();
+                }
+                else if ($scope.tabName === "You Tube") {
+                    searchYoutube();
+                }
+            };
+
+            $mdSidenav('right').open();
+            $scope.openRightMenu = function () {
+                $scope.documents = "";
+                $mdSidenav('right').toggle();
+            };
+            $scope.openRightMenu();
+
+            $scope.clearResults = function () {
+                $scope.documents = "";
+                $scope.showSearchOptions = false;
+                if ($scope.tabName === "The New York Times") {
+                    nytPageResetCounter += 1;
+                }
+                else if ($scope.tabName === "The Guardian") {
+                    guardianPageResetCounter += 1;
+                }
+            }
+}]);
--- src/main/resources/public/services/tabService.js
+++ src/main/resources/public/services/tabService.js
@@ -1,28 +1,29 @@
-"use strict";
+'use strict';
 
-angular.module("core")
-    .factory("tabService", ["$http", "apiKeys", function ($http, apiKeys) {
+angular.module('core')
+    .factory('tabService', ['$http', 'apiKeys', function ($http, apiKeys) {
         var tabService = this;
 
-        tabService.nytArticleSearch = function (searchText, pageNum) {
+        tabService.nytArticleSearch = function (searchText, pageNum, sortOrder) {
             var articleSearchEndPoint = "http://api.nytimes.com/svc/search/v2/articlesearch.json?";
-            articleSearchEndPoint += "q=" + searchText + "&hl=true&page=" + pageNum + "&api-key=" + apiKeys["nyt"];
+            articleSearchEndPoint += "&q=" + searchText + "&sort=" + sortOrder + "&hl=true&page=" + pageNum + "&api-key=" + apiKeys["nyt"];
             return $http.get(articleSearchEndPoint).then(function(response) {
                 return response;
             });
         };
 
-        tabService.guardianArticleSearch = function (searchText, pageNum) {
+        tabService.guardianArticleSearch = function (searchText, pageNum, sortOrder) {
             var articleSearchEndPoint = "http://content.guardianapis.com/search?";
-            articleSearchEndPoint += "api-key=" + apiKeys["guardian"] + "&page=" + pageNum + "&q=" + searchText + "&show-fields=trailText";
+            articleSearchEndPoint += "api-key=" + apiKeys["guardian"] + "&page=" + pageNum + "&q=" + searchText + "&order-by=" + sortOrder + "&show-fields=trailText";
             return $http.get(articleSearchEndPoint).then(function(response) {
                 return response;
             });
         };
 
-        tabService.youtubeSearch = function (searchText, type) {
+        tabService.youtubeSearch = function (searchText, type, sortOrder) {
             var videoSearchEndPoint = "https://www.googleapis.com/youtube/v3/search?part=snippet";
-            videoSearchEndPoint += "&maxResults=10" + "&order=relevance" + "&q=" + searchText + "&type=" + type + "&key=" + apiKeys["googleBrowserKey"];
+            videoSearchEndPoint += "&maxResults=10" + "&order=" + sortOrder + "&q=" + searchText + "&type=" + type + "&key=" + apiKeys["googleBrowserKey"];
+            console.log(videoSearchEndPoint);
             return $http.get(videoSearchEndPoint).then(function(response) {
                     return response;
             });
@@ -30,7 +31,6 @@ angular.module("core")
         tabService.hodSentimentAnalysis = function (url) {
             var sentimentAnalysisEndpoint = "https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?";
             sentimentAnalysisEndpoint += "url=" + url + "&apikey=" + apiKeys["hod"];
-            console.log(sentimentAnalysisEndpoint);
             return $http.get(sentimentAnalysisEndpoint).then(function(response) {
                 return response;
             });
